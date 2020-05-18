39acdd3037efeba64c67ec43338d51981ec23b2d
yo_kanyukari <e1n17075@st.oit.ac.jp>
2020-05-18 18:26:15 +0900

    make time-lapse

--- main.go
+++ main.go
@@ -3,26 +3,29 @@ package main
 import (
 	"archive/tar"
 	"bytes"
+	"fmt"
 	"io"
 	"io/ioutil"
-	"os"
 	"log"
-	"fmt"
+	"os"
+	"os/exec"
 )
 
-func main() {
+func unpackTar() {
 	var file *os.File
 	var err error
 
+	//tarのopen
 	if file, err = os.Open("test/200506-125.tar"); err != nil {
 		log.Fatalln(err)
 	}
 	defer file.Close()
 
-	// ReaderはClose()はない。
+	//tar fileにまとめられているfileの先頭のfileを受け取る
 	reader := tar.NewReader(file)
 
-	for i:=0;true;i++ {
+	//一つずつfileを作っていく
+	for i := 0; true; i++ {
 		_, err = reader.Next()
 		if err == io.EOF {
 			// ファイルの最後
@@ -37,8 +40,24 @@ func main() {
 			log.Fatalln(err)
 		}
 
-		if err = ioutil.WriteFile( fmt.Sprintf("test/source%4d.jpg",i)  , buf.Bytes(), 0755); err != nil {
+		if err = ioutil.WriteFile(fmt.Sprintf("test/source%04d.jpg", i), buf.Bytes(), 0755); err != nil {
 			log.Fatal(err)
 		}
 	}
+
+	fmt.Println("unpack tar")
+}
+
+func makeTimeLapse() {
+	err := exec.Command("ffmpeg", "-f", "image2", "-r", "3", "-i", "test/source%04d.jpg", "-r", "15", "-an", "-vcodec", "libx264", "-pix_fmt", "yuv420p", "test/video.mp4","-y").Run()
+	if err != nil {
+		log.Fatal(err)
+	}
+	
+	fmt.Println("make time-lapse")
+}
+
+func main() {
+	unpackTar()
+	makeTimeLapse()
 }
--- memo.md
+++ memo.md
@@ -3,3 +3,8 @@ tarの展開
 https://github.com/tyokoyama/golangcafe/blob/master/sample.com/archivesample/readtar.go
 
 
+golangでexec
+https://go言語.com/pkg/os/exec/
+
+ffmpeg
+https://qiita.com/riocampos/items/2f4fe927b5cf99aff767
