975edfb52cae110d174502fae12a50e952d5c6ed
yo_kanyukari <e1n17075@st.oit.ac.jp>
2020-05-19 14:47:10 +0900

    *1分にffmpegを実行するようにスケジュール機能の追加

--- main.go
+++ main.go
@@ -9,6 +9,7 @@ import (
 	"log"
 	"os"
 	"os/exec"
+	"time"
 )
 
 func unpackTar() {
@@ -49,15 +50,32 @@ func unpackTar() {
 }
 
 func makeTimeLapse() {
-	err := exec.Command("ffmpeg", "-f", "image2", "-r", "3", "-i", "test/source%04d.jpg", "-r", "15", "-an", "-vcodec", "libx264", "-pix_fmt", "yuv420p", "test/video.mp4","-y").Run()
+	unpackTar()
+
+	err := exec.Command("ffmpeg", "-f", "image2", "-r", "20", "-i", "test/source%04d.jpg", "-r", "40", "-an", "-vcodec", "libx264", "-pix_fmt", "yuv420p", "test/video.mp4", "-y").Run()
 	if err != nil {
 		log.Fatal(err)
 	}
-	
+
 	fmt.Println("make time-lapse")
 }
 
+func scheduler() {
+	ticker := time.NewTicker(30 * time.Second)
+	lastMinute := time.Now().Minute()
+	defer ticker.Stop()
+	for {
+		select {
+		case now := <-ticker.C:
+			fmt.Println(now.Format(time.RFC3339))
+			if now.Minute()%10 == 1 && now.Minute() != lastMinute {
+				makeTimeLapse()
+				lastMinute = now.Minute()
+			}
+		}
+	}
+}
+
 func main() {
-	unpackTar()
-	makeTimeLapse()
+	scheduler()
 }
--- memo.md
+++ memo.md
@@ -8,3 +8,10 @@ https://go言語.com/pkg/os/exec/
 
 ffmpeg
 https://qiita.com/riocampos/items/2f4fe927b5cf99aff767
+
+スケジューリングのためのticker
+https://text.baldanders.info/golang/ticker/
+
+日付を扱う
+https://ashitani.jp/golangtips/tips_time.html#time_Now
+https://qiita.com/wMETAw/items/2c3120d1338c646ecfba
