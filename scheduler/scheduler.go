10c7184ed4cf6c233586d3618db8695742be0e00
yo_kanyukari <e1n17075@st.oit.ac.jp>
2020-05-24 04:11:54 +0900

    CPU使いすぎないためスケジュールを減らし、四時にファイルがなかった

--- scheduler/scheduler.go
+++ scheduler/scheduler.go
@@ -10,14 +10,14 @@ import (
 // Scheduler 一定時間ごとに実行する
 func Scheduler() {
 	time.Local = time.FixedZone("JST", +9*60*60)
-	ticker := time.NewTicker(10 * time.Second)
+	ticker := time.NewTicker(30 * time.Second)
 	lastMinute := time.Now().Minute()
 	defer ticker.Stop()
 	for {
 		select {
 		case now := <-ticker.C:
 			fmt.Println(now)
-			if (now.Minute()%10 == 1&&4<=now.Hour()&&now.Hour()<8||now.Hour()==8&&now.Minute()==1) && now.Minute() != lastMinute {
+			if (now.Minute()%10 == 1&&5<=now.Hour()&&now.Hour()<8||now.Hour()==8&&now.Minute()==1) && now.Minute() != lastMinute {
 				s3.Getjpgtar(s3.S3TarPath(now),"jpg/jpg.tar")
 				timelapse.MakeTimeLapse()
 				lastMinute = now.Minute()
